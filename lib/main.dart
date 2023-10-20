6dcd684197476a4972cc08da9823d054a4eced49
Andrea Zollini <andrea.zollini@dgalab.it>
2023-10-20 17:39:33 +0200

    gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:basi_avanzate/gradient_container.dart';
 
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 255, 12, 78),
+          Color.fromARGB(255, 145, 65, 0),
+        ),
+      ),
+    ),
+  );
+}
