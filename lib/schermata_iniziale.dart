5a312eee8390d04746b736e8c86371aee9d4d9c8
andrea <andrea.zollini@proton.me>
2023-10-23 00:19:58 +0200

    schermata iniziale

--- lib/schermata_iniziale.dart
+++ lib/schermata_iniziale.dart
@@ -10,8 +10,28 @@ class SchermataIniziale extends StatelessWidget {
       children: [
         Image.asset(
           'assets/images/quiz-logo.png',
-          width: 200,
+          width: 300,
         ),
+        const SizedBox(
+          height: 80,
+        ),
+        const Text(
+          'Learn flutter the fun way!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.white,
+          ),
+          child: const Text('Start Quiz'),
+        )
       ],
     );
   }
