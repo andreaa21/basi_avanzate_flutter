10e19f3ab31e715f9380bc389a1cb626f51d2862
Andrea Zollini <andrea.zollini@dgalab.it>
2023-10-23 15:07:56 +0200

    domande e risposte dinamiche

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:basi_avanzate/answer_button.dart';
+import 'package:basi_avanzate/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -9,40 +11,46 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      if (currentQuestionIndex > 5) {
+        currentQuestionIndex = 0;
+      }
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Domanda',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 1'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 2'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 3'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 4'),
-          ),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answer,
+                answerQuestion,
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
--- lib/schermata_iniziale.dart
+++ lib/schermata_iniziale.dart
@@ -7,36 +7,40 @@ class SchermataIniziale extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 300,
-          color: const Color.fromARGB(150, 255, 255, 255),
-        ),
-        const SizedBox(
-          height: 80,
-        ),
-        const Text(
-          'Learn flutter the fun way!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        OutlinedButton.icon(
-          onPressed: startQuiz,
-          style: OutlinedButton.styleFrom(
-            foregroundColor: Colors.white,
+          const SizedBox(
+            height: 80,
           ),
-          icon: const Icon(Icons.arrow_right_alt),
-          label: const Text('Start Quiz'),
-        )
-      ],
+          const Text(
+            'Learn flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ],
+      ),
     );
   }
 }
